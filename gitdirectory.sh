225c9388d67dc27994ee93af303e6ebf12c08924
Akira Otaki <ohatakky@gmail.com>
2020-08-04 02:05:17 +0900

    fix

--- gitdirectory.sh
+++ gitdirectory.sh
@@ -2,6 +2,7 @@ clear
 list=($(git log --reverse | grep -e '^commit' | awk '{print $2}'))
 
 idx=0
+git checkout ${list[idx]} > /dev/null 2>&1 && tree -d
 while :
 do
   read -p "h or l : " action
@@ -12,10 +13,17 @@ do
   else
     continue
   fi
+
   if [ $(($idx)) -lt 0 ]; then
     idx=0
+    clear
+    continue
+  elif [ $(($idx)) -ge ${#list[@]} ]; then
+    idx=$((${#list[@]}-1))
+    clear
     continue
   fi
+
   clear
   git checkout ${list[idx]} > /dev/null 2>&1 && tree -d
 done
