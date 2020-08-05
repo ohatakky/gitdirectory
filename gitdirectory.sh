57ffae937c11a133ab0a1296c5fffa6a5364bf7d
Akira Otaki <ohatakky@gmail.com>
2020-08-05 22:34:02 +0900

    support: git clone none blob

--- gitdirectory.sh
+++ gitdirectory.sh
@@ -1,9 +1,27 @@
-clear
+spin()
+{
+    i=1
+    sp="/-\|"
+    while true
+    do
+        printf "\b${sp:i++%${#sp}:1}"
+    done
+}
+
+spin &
+SPIN_PID=$!
+trap "kill -9 $SPIN_PID" `seq 0 15`
+
+repo=git@github.com:$1.git
+git clone --filter=blob:none --no-checkout $repo > /dev/null 2>&1 && cd "$(echo $1 | sed -e 's/.*\///g')"
+
+kill -9 $SPIN_PID > /dev/null 2>&1
+
 list=($(git log --reverse | grep -e '^commit' | awk '{print $2}'))
 prURL=https://github.com/$(git config --get remote.origin.url | sed -e 's/git@github.com://g' | sed -e 's/.git//g')/pulls?q=is%3Apr+hash%3A
 
 idx=0
-git checkout ${list[idx]} > /dev/null 2>&1 && tree -d
+git ls-tree -d -r ${list[idx]} | awk '{print $4}' | sed -e 's;[^/]*/; |__;g;s;__|; |;g'
 while :
 do
     read -p "h or l or [0-9]+ or -[0-9]+: " action
@@ -32,5 +50,5 @@ do
     clear
     echo commitID: ${list[idx]}
     echo PR: $prURL${list[idx]}
-    git checkout ${list[idx]} > /dev/null 2>&1 && tree -d
+    git ls-tree -d -r ${list[idx]} | awk '{print $4}' | sed -e 's;[^/]*/; |__;g;s;__|; |;g'
 done
